df5dbf35b851f11fe196e42ed6def097b57f9c97
CristopherBustamante <173754@upslp.edu.mx>
2025-10-10 19:11:01 -0600

    feature/173754 update

--- src/train.py
+++ src/train.py
@@ -1,8 +1,12 @@
 from sklearn.datasets import fetch_20newsgroups
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.pipeline import make_pipeline
 from sklearn import metrics
+from src.model import create_model, train_model, predict
+from src.metrics import evaluate_metrics
+
+# Estos fetchers ahora se encuentran en model.py
+#from sklearn.feature_extraction.text import TfidfVectorizer
+#from sklearn.naive_bayes import MultinomialNB
+#from sklearn.pipeline import make_pipeline
 
 # Cargar datos
 categories = ['alt.atheism', 'comp.graphics', 'sci.space']
@@ -10,13 +14,29 @@ train = fetch_20newsgroups(subset='train', categories=categories)
 test = fetch_20newsgroups(subset='test', categories=categories)
 
 # Crear pipeline
-model = make_pipeline(TfidfVectorizer(), MultinomialNB())
+# usa la función de model.py
+model = create_model()
 
 # Entrenar
-model.fit(train.data, train.target)
+train_model(model, train.data, train.target)
 
 # Predecir
-predicted = model.predict(test.data)
+predicted = predict(model, test.data)
 
-# Evaluar
-print(metrics.classification_report(test.target, predicted, target_names=test.target_names))
+print("\n Reporte")
+print(metrics.classification_report(test.target, predicted, target_names=test.target_names, zero_division=0))
+
+accuracy = metrics.accuracy_score(test.target, predicted)
+precision_macro, recall_macro, f1_macro = evaluate_metrics(test.target, predicted, average='macro')
+precision_weighted, recall_weighted, f1_weighted = evaluate_metrics(test.target, predicted, average='weighted')
+
+# Evaluar métricas
+precision, recall, f1 = evaluate_metrics(test.target, predicted, average='macro')
+
+print("Métricas de Evaluacion")
+print(f"Precision: {precision:.4f}")
+print(f"Recall: {recall:.4f}")
+print(f"F1-Score: {f1:.4f}")
+print(f"Accuracy (Exactitud): {accuracy:.4f}")
+print(f"Macro Avg -> Precision: {precision_macro:.4f}, Recall: {recall_macro:.4f}, F1: {f1_macro:.4f}")
+print(f"Weighted Avg -> Precision: {precision_weighted:.4f}, Recall: {recall_weighted:.4f}, F1: {f1_weighted:.4f}")
