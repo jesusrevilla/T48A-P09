99cef881085220dad4960f443323bb4ccfd75412
armekmc54-ops <armekmc54@gmail.com>
2025-10-10 19:47:16 -0600

    Update model.py

--- src/model.py
+++ src/model.py
@@ -3,13 +3,16 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
 def create_model():
-    model = make_pipeline(TfidVectorizer(), MultinomialNB())
+ 
+    model = make_pipeline(TfidfVectorizer(), MultinomialNB())
     return model
 
 def train_model(model, data, target):
+  
     model.fit(data, target)
     return model
 
 def predict(model, data):
+    
     predictions = model.predict(data)
     return predictions
