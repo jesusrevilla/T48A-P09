e2dd2abc17c48b8d16669821567e7cbfd8155c46
GiselaM12 <100981841+GiselaM12@users.noreply.github.com>
2025-10-10 19:28:04 -0600

    Update metrics.py

--- src/metrics.py
+++ src/metrics.py
@@ -1,15 +1,8 @@
 from sklearn.metrics import precision_score, recall_score, f1_score
 
 def evaluate_metrics(y_true, y_pred, average='macro'):
-    
     precision = precision_score(y_true, y_pred, average=average)
     recall = recall_score(y_true, y_pred, average=average)
     f1 = f1_score(y_true, y_pred, average=average)
 
-    metrics_dict = {
-        'precision': precision,
-        'recall': recall,
-        'f1_score': f1
-    }
-
-    return metrics_dict
+    return precision, recall, f1
