2b52e3cf9ccdab12f8c4443a27f2ab093182c55f
karla176336 <176336@upslp.edu.mx>
2025-10-10 19:43:01 -0600

    Update metrics.py

--- src/metrics.py
+++ src/metrics.py
@@ -1,4 +1,4 @@
 from sklearn.metrics import precision_score, recall_score, f1_score
 
 def evaluate_metrics(y_true, y_pred, average='macro'):
-    pass
+    print(metrics.classification_report(test.target, predicted, target_names=test.target_names))
