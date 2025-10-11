991b63ad4cb2e1342d0a558107cbdc4e2d992de1
armekmc54-ops <armekmc54@gmail.com>
2025-10-10 19:40:22 -0600

    Update metrics.py

--- src/metrics.py
+++ src/metrics.py
@@ -1,4 +1,13 @@
 from sklearn.metrics import precision_score, recall_score, f1_score
 
 def evaluate_metrics(y_true, y_pred, average='macro'):
-    pass
+    precision = presicion_score(y_true, y_pred, average=average)
+    recall = recall_score(y_true, y_pred, average=avearge)
+    f1 = f1_score(y_true, y_pred, average=average)
+
+return {
+    'presicion' : precision,
+    'recall': recall,
+    'f1_score': f1
+}
+
